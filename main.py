9a828539154063db6701df8b963202582f101c53
Bruce Paulson <bpaulson@students.avc.edu>
2025-04-11 13:58:53 -0700

    need to fix total to not sum over 100

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 
-
 def main():
     total = 0
     numbers = []
+
     """
     ########################################
     Code Your Program here
@@ -13,18 +13,17 @@ def main():
         number = random.randint(1, 10)
         numbers.append(number)
         total += number
-    
+
+    # The loop exits AFTER total > 100, so the last number pushed it over
     sum_numbers_exclude_last = sum(numbers[:-1])
 
-
     print(f'The random values are {numbers}')
-    print(f'The total is {total}')
+    print(f'The total is {sum_numbers_exclude_last}')
 
     ########################################
     # Do not delete the return statement
     ########################################
-    return numbers, total
-
+    return numbers, sum_numbers_exclude_last
 
 if __name__ == '__main__':
     main()
